19320e2d32ed9678ff1bf77d74ffbb555e38bd2e
Nikoloz-Lortkipanidze <nikusha191208@gmail.com>
2025-02-27 16:37:09 +0400

    used every angular binding!

--- homework1/src/app/app.component.ts
+++ homework1/src/app/app.component.ts
@@ -8,5 +8,32 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'homework1';
+  country = 'Georgia'
+  city = 'Tbilisi'
+  date = 'February 27'
+
+  temperature = 23
+  nextFormat = "Fahrenheit"
+
+  lightMode = true
+  backgroundColor = 'lightgray'
+  headerColor = 'black'
+  headerTextColor = 'white'
+
+  getTemperatureFahrenheit() {
+    if (this.nextFormat == "Fahrenheit") {
+      this.nextFormat = "Celcius"
+      this.temperature = Math.round((this.temperature * 9/5 + 32) * 10) / 10
+    } else {
+      this.nextFormat = "Fahrenheit"
+      this.temperature = Math.round(((this.temperature - 32) * 5/9) * 10) / 10
+    }
+  }
+
+  switchMode() {
+    this.lightMode = !this.lightMode
+    this.backgroundColor = this.lightMode ? "lightgray" : "darkgray"
+    this.headerColor = this.lightMode ? "black" : "white"
+    this.headerTextColor = this.lightMode ? "white" : "black"
+  }
 }
